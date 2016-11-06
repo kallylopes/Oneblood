0ace80dff9db9e744568d8f3ad96ecd7e8a9dd97
I-am-Gabi <gabicavalcantesilva@gmail.com>
2016-11-06 02:16:31 -0300

    fix bug with questions logic

--- oneblood.py
+++ oneblood.py
@@ -5,9 +5,7 @@ from flask import Flask, render_template, request, session
 from forms import QuizzForm, SexForm, RegistrationQuizzForm
 from flask_sqlalchemy import SQLAlchemy
 
-import logging
-import json
-
+import models
 
 app = Flask(__name__)
 app.secret_key = 's3cr3t'
@@ -17,78 +15,51 @@ db = SQLAlchemy(app)
 
 list_quizz = []
 current_quizz = None
-
-
-class Quizz(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(800))
-    man_temp = db.Column(db.Integer)
-    woman_temp = db.Column(db.Integer)
-    status = db.Column(db.Integer)
-
-    def __init__(self, question, man_temp, woman_temp, status):
-        self.question = question.encode('utf8')
-        self.man_temp = man_temp
-        self.woman_temp = woman_temp
-        self.status = status
-
-    def __repr__(self):
-        obj_json = '{"question": %r }' % self.question
-        return json.dumps(obj_json)
-
-    def __eq__(self, other):
-        if not isinstance(other, Quizz):
-            return False
-        return self.id == other.id
+final_temp = 0
 
 
 @app.route('/', methods=['post', 'get'])
 def index():
-    global list_quizz, current_quizz
+    global list_quizz, current_quizz, final_temp
+
     if request.method == 'POST':
-        if len(list_quizz) == 0:
-            return render_template('index.html', temp=session['final_temp'])
+        if not list_quizz:
+            return render_template('index.html', temp=final_temp)
 
         form = QuizzForm()
 
         if 'sex' not in session:
             session['sex'] = form.opt.data
-            app.logger.info('Sex {}'.format(session['sex']))
-
-            # session['current_quizz'] = list_quizz.pop()
             current_quizz = list_quizz.pop()
-            # app.logger.info(session['current_quizz'])
+            label = current_quizz.question
+            return render_template('index.html', form=form, label=label)
 
-            label = current_quizz.question # session['current_quizz'].question
-            app.logger.info(label)
-
-        elif form.opt.data:
+        if bool(form.opt.data):
+            app.logger.info(form.opt.data)
             if session['sex'] == 0:
-                # t = session['current_quizz'].woman_temp
                 t = current_quizz.woman_temp
             else:
-                # t = session['current_quizz'].man_temp
-                t = current_quizz
-            if t == -1 or t > session['final_temp']:
-                session['final_temp'] = t
+                t = current_quizz.man_temp
+            if t == -1 or t > final_temp:
+                final_temp = t
 
-            while True:
-                if len(list_quizz) == 0:
-                    return render_template('index.html', temp=session['final_temp'])
-                current_quizz = list_quizz.pop()
-                label = current_quizz.question
-                if current_quizz.status != session['sex']:
-                    continue
+        while True:
+            if not list_quizz:
+                return render_template('index.html', temp=final_temp)
+            current_quizz = list_quizz.pop()
+            label = current_quizz.question
+            if current_quizz.status != session['sex'] and current_quizz.status != 2:
+                continue
+            else:
+                break
     else:
         app.logger.info('Index page')
 
         form = SexForm()
         label = form.opt.label
-        list_quizz = Quizz.query.all()
+        list_quizz = models.Quizz.query.all()
 
-        session['final_temp'] = 0
-        session.pop('final_temp', None)
-        # session.pop('current_quizz', None)
+        final_temp = 0
         session.pop('sex', None)
 
     return render_template('index.html', form=form, label=label)
@@ -98,10 +69,10 @@ def index():
 def players():
     form = RegistrationQuizzForm(request.form)
     if request.method == "POST":
-        q = Quizz(form.question.data, form.man_temp.data, form.woman_temp.data, form.status.data)
+        q = models.Quizz(form.question.data, form.man_temp.data, form.woman_temp.data, form.status.data)
         db.session.add(q)
         db.session.commit()
-    return render_template('quizzes.html', form=form, quizzes=Quizz.query.all())
+    return render_template('quizzes.html', form=form, quizzes=models.Quizz.query.all())
 
 
 @app.route('/delete', methods=['POST', 'GET'])
@@ -110,9 +81,9 @@ def delete():
 
     if request.method == "POST":
         id = int(request.form['id'])
-        db.session.delete(Quizz.query.get(id))
+        db.session.delete(models.Quizz.query.get(id))
         db.session.commit()
-    return render_template('quizzes.html', form=form, quizzes=Quizz.query.all())
+    return render_template('quizzes.html', form=form, quizzes=models.Quizz.query.all())
 
 
 if __name__ == '__main__':
